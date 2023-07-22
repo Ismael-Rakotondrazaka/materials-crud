6452aaaf1546428d6d9b48d56eb211d2c92ab8e1
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-07-22 10:13:03 +0300

    feat(materials.js): Add auto init with fake datas
    fix(HomeView.vue): Don't initialize materials store when mounted

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,8 @@
 import MaterialList from "../components/materials/MaterialList.vue";
 import MaterialStatusChart from "../components/materials/MaterialStatusChart.vue";
 import { useMaterialStore } from "../stores/material.js";
-import { onMounted } from "vue";
 
 const materialStore = useMaterialStore();
-
-// when the component is mounted
-onMounted(() => {
-  // get all materials and put them into the store
-  materialStore.getMaterials().then((materials) => {
-    materialStore.materials = materials;
-  });
-});
 </script>
 
 <template>
